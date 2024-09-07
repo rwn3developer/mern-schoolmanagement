fca64d71b46da668562ffec180e9920d2064f05c
milansir <rwn3developer11@gmail.com>
2024-09-07 12:41:22 +0530

    school management

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h2>Hello world</h2>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const port = 8000;
+
+const app = express();
+
+app.listen(port,(err)=>{
+    if(err){
+        console.log(err);
+        return false
+    }
+    console.log(`server is start on port :- ${port}`);
+    
+})
